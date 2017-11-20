SlimRoms/hardware_qcom_keymaster

keymaster: add msm8998 into support list
Change-Id: Iaa173c669d3ac3acc39d65e9e456879c7eb25755
Signed-off-by: Martinusbe <martinusbe@gmail.com>

Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
-ifneq ($(filter msm8909 msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8909 msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8916 msm8952 msm8992 msm8994 msm8998,$(TARGET_BOARD_PLATFORM)),)
 
 keymaster-def := -fvisibility=hidden -Wall
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)

AospExtended/platform_hardware_qcom_keymaster

Android.mk
@@ -3,7 +3,7 @@ ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
-ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994 msm8998,$(TARGET_BOARD_PLATFORM)),)
 
 keymaster-def := -fvisibility=hidden -Wall
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)